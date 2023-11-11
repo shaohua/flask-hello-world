d07d5f4b078689fe6cc2d03052f72041dd6d2385
shaohua <shaohua@users.noreply.github.com>
2023-11-10 23:31:11 -0800

    Add get_page

--- app.py
+++ app.py
@@ -4,3 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/get_page')
+def get_page():
+    return {
+        "page": "<H1>Page</H1>"
+    }
